d8498b082ed455405a5bba08e84d8147bb9b8cd9
yx53074 <yx53074@163.com>
2019-04-11 16:04:02 +0800

    111

--- src/entries/my/container/index.js
+++ src/entries/my/container/index.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from "react";
-import { View, Text, WebView } from "react-native";
+import { View, Text, WebView, Platform } from "react-native";
 
 export default class Index extends PureComponent {
     constructor() {
@@ -8,7 +8,9 @@ export default class Index extends PureComponent {
             uri: ""
         };
     }
-    jsCode = `window.isApp=true;window.localStorage.setItem('isApp',true);window.localStorage.setItem('token','eyJpdiI6Ik1ERXdNakF6TURRd05UQTJNRGN3T0E9PSIsInZhbHVlIjoiK3E1YkhjVEUrOGxaTmNsdzdZbDM5K0h2bzEwbGs4SjgydjdhTk9JREJVdzNaZ3RtWUc1bVozQnBPZDd3UXBCWm5jTG54d1ZpR2JSRkM1U0dQZlo3SGd1a05qWHFScjgxek5salRBcGpiTzgzNEtFSXRTSisxS29Gb2M0bDRqMmRNT0NrQmhDOWZBZWVzTTU1ajk4K2hFYW0zcnN5YWZBTDFkV1d3ak02NGtTQ0U1R3gxMzRrOEptZHRncVlvOUo4ODNiVFRNTVJmcnc0TVRnSUJ1Q0tMbGowYXhPa1ZQSEhJV05sWE1vWUwxTUQvRVRJOW8zV1ZtRFNPRU1MTWIrU3VsQkR5bStFYWQ5Y1ZjMEVnWUJ6YWVmM2MyRUUyNnZuUFNOT3UyZmZiWnl0T2dQM3I0bDlEdkxKbWhaMHBGTTlKa25mQ2FoRjZGRURkRXlMTEc5cE16eFpvZUp1dHl0dlhuS3lEcGJ3VDQ1eGlRb0EyTVNyeEdGM1A5K2NodXJnK1J4YWVDQTlKUkdsOGI5SlRidXU0WFdIY3JvUnJXRHhEWWUwcmtXUm1lZ1JjOVpKbTN4Z3Jxd1pDOWZwK3JwdCJ9');`;
+    jsCode = `window.localStorage.setItem('isApp',false);window.localStorage.setItem('platform','${
+        Platform.OS == "ios" ? "iOS" : "Android"
+    }');window.localStorage.setItem('isApp',true);window.localStorage.setItem('token','eyJpdiI6Ik1ERXdNakF6TURRd05UQTJNRGN3T0E9PSIsInZhbHVlIjoiK3E1YkhjVEUrOGxaTmNsdzdZbDM5K0h2bzEwbGs4SjgydjdhTk9JREJVdzNaZ3RtWUc1bVozQnBPZDd3UXBCWm5jTG54d1ZpR2JSRkM1U0dQZlo3SGd1a05qWHFScjgxek5salRBcGpiTzgzNEtFSXRTSisxS29Gb2M0bDRqMmRNT0NrQmhDOWZBZWVzTTU1ajk4K2hFYW0zcnN5YWZBTDFkV1d3ak02NGtTQ0U1R3gxMzRrOEptZHRncVlvOUo4ODNiVFRNTVJmcnc0TVRnSUJ1Q0tMbGowYXhPa1ZQSEhJV05sWE1vWUwxTUQvRVRJOW8zV1ZtRFNPRU1MTWIrU3VsQkR5bStFYWQ5Y1ZjMEVnWUJ6YWVmM2MyRUUyNnZuUFNOT3UyZmZiWnl0T2dQM3I0bDlEdkxKbWhaMHBGTTlKa25mQ2FoRjZGRURkRXlMTEc5cE16eFpvZUp1dHl0dlhuS3lEcGJ3VDQ1eGlRb0EyTVNyeEdGM1A5K2NodXJnK1J4YWVDQTlKUkdsOGI5SlRidXU0WFdIY3JvUnJXRHhEWWUwcmtXUm1lZ1JjOVpKbTN4Z3Jxd1pDOWZwK3JwdCJ9');`;
     componentDidMount() {
         this.setState({
             uri:
